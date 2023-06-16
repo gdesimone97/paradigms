832e77c55e4be994dcc7050462a710220f1436bc
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-06-16 11:01:23 +0200

    up

--- logic/derivative.pl
+++ logic/derivative.pl
@@ -3,20 +3,26 @@ der(sin(X),X,cos(X)).
 der(cos(X),X,-sin(X)).
 der(tan(X),X,1/cos(X)*cos(X)).
 
-%Constant
+der(-F, X, -DF):-
+    der(F, X, DF).
+
 der(C,X,0):-
+    C \= X,
     atomic(C),
     atomic(X).
 
-%Power
 der(X^1,X,1).
-der(X,X,1) :- der(X^1,X,1). %alias
+der(X,X,1) :- der(X^1,X, 1). %alias
 der(X^C, X, DF):-
     number(C),
     DIFF is C -1,
     DF = C * X^[DIFF];
     nonvar(C),
     DF = C * X^[C-1].
+
+der(C*X, X, C*DF):-
+    atomic(C),
+    der(X, X, DF).
 
 der(X^C, Y, 0):-
     atomic(C),
