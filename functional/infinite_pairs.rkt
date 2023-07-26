03db2d4c282ab6f84687fcd15fa2bed28d79e4a7
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-07-26 11:57:57 +0200

    Create infinite_pairs.rkt

---
+++ functional/infinite_pairs.rkt
@@ -0,0 +1,50 @@
+#lang scheme
+(define empty-stream '())
+
+(define-syntax cons-stream
+ (syntax-rules ()
+ ( (cons-stream expr1 expr2)
+ (cons expr1 (delay expr2)) )))
+
+(define stream-null? null?)
+(define stream-car car)
+(define (stream-cdr s) (force (cdr s)))
+(define (stream-cadr s) (stream-car (stream-cdr s)))
+(define (stream-cddr s) (stream-cdr (stream-cdr s)))
+
+(define (take n stream)
+  (if (or (stream-null? stream) (< n 1))
+          '()
+       (cons (stream-car stream)
+       (take (- n 1) (stream-cdr stream)))))
+
+(define (stream-map func s)
+  (if (stream-null? s)
+      '()
+      (cons-stream (func (stream-car s)) (stream-map func (stream-cdr s)))))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+(define test (pairs integers integers))
+(take 20 test)
+
+
+
+
