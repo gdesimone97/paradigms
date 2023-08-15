f23cc530cd3fc589f1f9b1f341b12ce86c9785ac
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-08-15 19:14:19 +0200

    Create queen.pl

---
+++ logic/queen.pl
@@ -0,0 +1,32 @@
+%! create(+DimTuple)
+%
+% The create/1 predicate succeeds if the DimTuple contains valid chessboard 
+% dimensions, e.g. (0,0) or (2,4).
+create((DimX, DimY)) :-
+	DimX < 9,
+    DimY < 9,
+    DimX > -1,
+    DimY > -1;
+    fail.
+
+%! attack(+FromTuple, +ToTuple)
+%
+% The attack/2 predicate succeeds if a queen positioned on ToTuple is 
+% vulnerable to an attack by another queen positioned on FromTuple.
+attack((FromX, FromY), (ToX, ToY)):-
+	X is FromX - ToX,
+    Y is FromY - ToY,
+    X =:= Y.
+
+attack((FromX, FromY), (ToX, ToY)):-
+    FromX =:= ToX.
+
+attack((FromX, FromY), (ToX, ToY)):-
+    FromY =:= ToY.
+
+attack((FromX, FromY), (ToX, ToY)):-
+    X is FromX - ToX,
+    Y is FromY - ToY,
+    T is 8 + 1,
+    S is X + Y,
+    T =:= S.
