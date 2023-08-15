e8c2ae4c4daaaf0b637b6a3d109f4930b1f175cc
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-08-15 19:14:16 +0200

    amb

--- functional/amb.rkt
+++ functional/amb.rkt
@@ -1,56 +1,29 @@
 #lang scheme
 
-(define current-continuation
-  (lambda ()
-    (call/cc (lambda (cc)
-               ;; `cc` will be the value of `k` in `amb`.
-               (cc cc)))))
+(define fail 
+  (lambda () 
+    (error "Amb tree exhausted"))) 
+  
+(define-syntax amb
+  (syntax-rules ()
+    ((_) (fail))
+    ((_ a) a)
+    ((_ a b ...)
+     (let ((fail0 fail))
+       (call/cc
+    (lambda (cc)
+      (set! fail
+        (lambda ()
+          (set! fail fail0)
+          (cc (amb b ...))))
+      (cc a)))))))      
 
-(define fail-stack '())                 ; list of continuations
-
-(define (fail)
-  (define test fail-stack)
-  (define test2 (car fail-stack))
-  (if (not (pair? fail-stack))
-      (error "back-tracking stack exhausted!")
-      (begin
-        (let ((back-track-point (car fail-stack)))
-          (set! fail-stack (cdr fail-stack))
-          (back-track-point back-track-point)))))
-
-(define amb
-  (lambda (choices)
-    ((lambda (k)
-       (if (null? choices)
-           (fail)
-           ((lambda (choice remaining-choices)
-              (set! choices remaining-choices)
-              (set! fail-stack (cons k fail-stack))
-              choice)
-            (car choices)
-            (cdr choices))))
-     (current-continuation))))
-
-(define (assert condition)
-  (if (not condition)
+(define (require condition) 
+  (if (not condition) 
       (fail)
-      #t))  
+      (display "")))
 
-; The following prints (4 3 5)
-(let ((a (amb (list 1 2 3 4 5 6 7)))
-      (b (amb (list 1 2 3 4 5 6 7)))
-      (c (amb (list 1 2 3 4 5 6 7))))
-
-  ; We're looking for dimensions of a legal right
-  ; triangle using the Pythagorean theorem:
-  (assert (= (* c c) (+ (* a a) (* b b))))
-
-  (display (list a b c))
-  (newline)
-
-  ; And, we want the second side to be the shorter one:
-  (assert (< b a))
-
-  ; Print out the answer:
-  (display (list a b c))
-  (newline))
+(let ((a (amb 1 2)) 
+      (b (amb 2 3 4))) 
+  (require (= a (+ a b)))
+  (list a b))
