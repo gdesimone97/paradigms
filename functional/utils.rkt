98313ac0fde6b8beba0180c544b9cd9db185fc72
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-11-09 16:29:52 +0100

    up utils

--- functional/utils.rkt
+++ functional/utils.rkt
@@ -22,13 +22,24 @@
                (cons proc (map stream-cdr argstreams))))))
 
 (define (stream-filter pred stream)
-  (cond ((stream-null? stream) the-empty-stream)
+  (cond ((stream-null? stream) empty-stream)
         ((pred (stream-car stream))
          (cons-stream (stream-car stream)
                       (stream-filter
                        pred
                        (stream-cdr stream))))
         (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (add-stream s1 s2)
+  (stream-map + s1 s2))
+
+(define (partial-sums s)
+  (cons-stream (car s) (add-stream (partial-sums s) (stream-cdr s))))
 
 (define (take n stream)
   (if (or (stream-null? stream) (< n 1))
